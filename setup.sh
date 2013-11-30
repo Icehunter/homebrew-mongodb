abaf4550eed24cabb7e859887b880151cf36111d
Ryan Wilson <syndicated.life@gmail.com>
2013-11-30 00:13:41 -0800

    initial commit

---
+++ setup.sh
@@ -0,0 +1,51 @@
+#!/bin/env
+launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.mongodb1.plist
+launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.mongodb2.plist
+launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.mongodb3.plist
+
+rm -f ~/Library/LaunchAgents/homebrew.mxcl.mongodb*.plist
+
+cp -f *.plist /usr/local/opt/mongodb/
+cp -f *.conf /usr/local/etc/
+
+mkdir -p /usr/local/var/log/mongodb1
+mkdir -p /usr/local/var/log/mongodb2
+mkdir -p /usr/local/var/log/mongodb3
+mkdir -p /usr/local/var/mongodb1
+mkdir -p /usr/local/var/mongodb2
+mkdir -p /usr/local/var/mongodb3
+
+ln -sfv /usr/local/opt/mongodb/homebrew.mxcl.mongodb*.plist ~/Library/LaunchAgents/
+
+launchctl load ~/Library/LaunchAgents/homebrew.mxcl.mongodb1.plist
+launchctl load ~/Library/LaunchAgents/homebrew.mxcl.mongodb2.plist
+launchctl load ~/Library/LaunchAgents/homebrew.mxcl.mongodb3.plist
+
+mongo 127.0.0.1:27017 <<EOF
+
+var cfg = {
+    "_id": "rs",
+    "version": 1,
+    "members": [
+        {
+            "_id": 0,
+            "host": "127.0.0.1:27017",
+            "priority": 1
+        },
+        {
+            "_id": 1,
+            "host": "127.0.0.1:27018",
+            "priority": .5
+        },
+        {
+            "_id": 2,
+            "host": "127.0.0.1:27019",
+            "priority": .5
+        }
+    ]
+};
+
+rs.initiate(cfg);
+rs.reconfig(cfg)
+
+EOF
